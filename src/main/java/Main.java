be47ab82dfc8fb9e3722364df284292d4542349e
Igor Khomenko <xomigrek@gmail.com>
2022-07-12 21:54:37 +0300

    UPD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import ru.netilogy.sqr.services.SQRService;
 public class Main {
     public static void main(String[] args) {
         SQRService service = new SQRService();
-        System.out.println(service.numSqr(100,130));
+        System.out.println(service.numSqr(100, 130));
     }
 }
